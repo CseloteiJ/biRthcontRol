566faf78332fee5257b39a526abf57e7c07e456c
kdaya-g <35219310+kdaya-g@users.noreply.github.com>
2018-01-16 15:02:44 +0100

    lkjfdslkjdsf

--- Rcode.R
+++ Rcode.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 library('tidyverse')
 library('ggplot2')
 
@@ -6,23 +7,39 @@ bdays = read.csv('birthdates.csv', header=FALSE)
 
 names(bdays) = c("months")
 
-
-bmonth = bdays %>%
-  filter(months %in% range(1,13))
-
 dataframe <- as.data.frame(table(bdays))
 
+meanfreq = mean(dataframe$Freq)
+deviation = sd(dataframe$Freq)
 
-ggplot(data = dataframe)+
+dataframe$bdays = as.integer(dataframe$bdays)
+dataframe$bdays = as.factor(dataframe$bdays)
+
+dataframe$bdays[0]
+
+ggplot()+
   geom_bar(mapping = aes(x=dataframe$bdays, y=dataframe$Freq), stat='summary', fun.y=mean)+
-  geom_abline(mapping = aes(mean(dataframe$Freq)))+
-error.bars(x,stats=NULL, ylab = "Dependent Variable",xlab="Independent Variable",
-             main=NULL,eyes=TRUE, ylim = NULL, xlim=NULL,alpha=.05,sd=FALSE, labels = NULL, 
-             pos = NULL,  arrow.len = 0.05,arrow.col="black", add = FALSE,bars=FALSE,within=FALSE,
-             col="blue",...)
+  geom_hline(yintercept = meanfreq, colour = "red", lwd = 1)+
+  geom_hline(yintercept = meanfreq - deviation)+
+  geom_hline(yintercept = meanfreq + deviation)+
+  xlab("Month of Birth")+
+  ylab("Frequency")+
+  ggtitle("Frequency of Births per Month as Registered on Wikipedia")
 
 
+dataframe.for.bitter.nonjanuary.losers = dataframe %>%
+  filter(dataframe$bdays >= 2)
 
-ggplot(data = bdays)+
-  geom_bar(mapping = aes(x=months), stat='summary', fun.y=mean)+
-  abline(mean())
+dataframe.for.bitter.nonjanuary.losers$bdays = as.factor(dataframe.for.bitter.nonjanuary.losers$bdays)
+
+meanfreqnonjan = mean(dataframe.for.bitter.nonjanuary.losers$Freq)
+deviationnonjan = sd(dataframe.for.bitter.nonjanuary.losers$Freq)
+
+ggplot()+
+  geom_bar(mapping = aes(x=dataframe.for.bitter.nonjanuary.losers$bdays, y=dataframe.for.bitter.nonjanuary.losers$Freq), stat='summary', fun.y=mean)+
+  geom_hline(yintercept = meanfreqnonjan, colour = "red", lwd = 1)+
+  geom_hline(yintercept = meanfreqnonjan - deviationnonjan)+
+  geom_hline(yintercept = meanfreqnonjan + deviationnonjan)+
+  xlab("Month of Birth")+
+  ylab("Frequency")+
+  ggtitle("Frequency of Births per Month as Registered on Wikipedia")
