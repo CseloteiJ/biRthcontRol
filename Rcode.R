68d690a3e1305e004d8f8cef3ac8fc1b3898aa50
kdaya-g <35219310+kdaya-g@users.noreply.github.com>
2018-01-17 16:45:13 +0100

    so many graphs sooo many graphs

--- Rcode.R
+++ Rcode.R
@@ -1,45 +1,61 @@
-<<<<<<< HEAD
+
 library('tidyverse')
 library('ggplot2')
 
+reallife = read.csv('livebirths.csv', header=FALSE)
 
-bdays = read.csv('birthdates.csv', header=FALSE)
+days = read.csv('birthdays.csv', header=FALSE)
 
-names(bdays) = c("months")
+days$V1 = as.Date(as.character(days$V1), format ='%m-%d')
+days$month = as.integer(format(days$V1, format = '%m'))
 
-dataframe <- as.data.frame(table(bdays))
+dataframe <- as.data.frame(table(days$month))
+
+reallife$percent = reallife$V2/sum(reallife$V2)
+dataframe$percent = dataframe$Freq/sum(dataframe$Freq)
 
 meanfreq = mean(dataframe$Freq)
 deviation = sd(dataframe$Freq)
 
-dataframe$bdays = as.integer(dataframe$bdays)
-dataframe$bdays = as.factor(dataframe$bdays)
+dataframe$diff = dataframe$percent - reallife$percent
 
-dataframe$bdays[0]
+meandiff = mean(dataframe$diff)
+diffdeviation = sd(dataframe$diff)
 
-ggplot()+
-  geom_bar(mapping = aes(x=dataframe$bdays, y=dataframe$Freq), stat='summary', fun.y=mean, fill = "black")+
-  geom_hline(yintercept = meanfreq, colour = "red", lwd = 1)+
-  geom_hline(yintercept = meanfreq - deviation)+
-  geom_hline(yintercept = meanfreq + deviation)+
+names = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+months <- dataframe$Var1
+dataframe2 = data.frame(cbind(dataframe$Freq),names,months)
+
+ggplot(dataframe2, aes(names, dataframe$Freq)) +   
+  geom_bar(aes(fill = months), position = "dodge", stat="identity") + scale_x_discrete(limits=names) +
+  geom_hline(yintercept = meanfreq, colour = "red", lwd = 1) +
+  geom_hline(yintercept = meanfreq - deviation, colour = "black")+
+  geom_hline(yintercept = meanfreq + deviation, colour = "black")+
+  xlab("Month of Birth") +
+  ylab("Frequency") +
+  ggtitle("Frequency of Births per Month as Registered on Wikipedia") +
+  labs(fill='Number of Month in Year')
+
+ggplot(dataframe2, aes(x="", y=dataframe$Freq, fill=months))+
+  geom_bar(width = 1, stat = "identity")+ 
+  scale_y_reverse()+
+  coord_polar("y")+
+  ylab("Relative Frequency of Birth")+
+  labs(fill='Number of Month in Year')
+
+ggplot(dataframe2, aes(x="", y=reallife$V2, fill=months))+
+  geom_bar(width = 1, stat = "identity")+ 
+  scale_y_reverse()+
+  coord_polar("y")+
+  ylab("Relative Frequency of Birth")+
+  labs(fill='Number of Month in Year')
+
+ggplot(dataframe2, aes(x=dataframe$Var1, y=dataframe$diff))+
+  geom_bar(mapping = aes(x=dataframe$Var1, y=dataframe$diff, fill = dataframe$Var1), stat='summary', fun.y=mean)+
+  geom_hline(yintercept = meandiff, colour = "red", lwd = 1)+
+  geom_hline(yintercept = meandiff - diffdeviation)+
+  geom_hline(yintercept = meandiff + diffdeviation)+
   xlab("Month of Birth")+
-  ylab("Frequency")+
-  ggtitle("Frequency of Births per Month as Registered on Wikipedia")
-
-
-dataframe.for.bitter.nonjanuary.losers = dataframe %>%
-  filter(dataframe$bdays >= 2)
-
-dataframe.for.bitter.nonjanuary.losers$bdays = as.factor(dataframe.for.bitter.nonjanuary.losers$bdays)
-
-meanfreqnonjan = mean(dataframe.for.bitter.nonjanuary.losers$Freq)
-deviationnonjan = sd(dataframe.for.bitter.nonjanuary.losers$Freq)
-
-ggplot()+
-  geom_bar(mapping = aes(x=dataframe.for.bitter.nonjanuary.losers$bdays, y=dataframe.for.bitter.nonjanuary.losers$Freq), stat='summary', fun.y=mean)+
-  geom_hline(yintercept = meanfreqnonjan, colour = "red", lwd = 1)+
-  geom_hline(yintercept = meanfreqnonjan - deviationnonjan)+
-  geom_hline(yintercept = meanfreqnonjan + deviationnonjan)+
-  xlab("Month of Birth")+
-  ylab("Frequency")+
-  ggtitle("Frequency of Births per Month as Registered on Wikipedia")
+  ylab("Difference in Relative Distributions of Births")+
+  ggtitle("Difference in Relative Distributions of Births, Wikipedia vs UN Live Births")+
+  labs(fill='Number of Month in Year')
